b31a6982301f1c7966cf11bf2de897380900e448
lucascicero4 <lucascicero444@gmail.com>
2026-02-12 22:36:44 -0300

    Update file paths in service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'mi-finanzas-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
@@ -13,13 +13,13 @@ self.addEventListener('fetch', e => {
         caches.open(CACHE).then(c => c.put(e.request, clone));
       }
       return res;
-    })).catch(() => caches.match('/index.html'))
+    })).catch(() => caches.match('./index.html'))
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    .then(() => self.clients.claim())
   );
 });
