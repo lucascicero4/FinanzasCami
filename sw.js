40a018df1de6d31403cf6dad3baafae212afe2fd
lucascicero4 <lucascicero444@gmail.com>
2026-02-20 00:17:19 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-finanzas-v4';
+const CACHE = 'mi-finanzas-v5';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
